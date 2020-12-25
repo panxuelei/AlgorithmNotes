5721d17bf8c1bf02fdbe0755d0dfbe7839cd0a66
Aaron Swartz <317118173pxl@gmail.com>
2020-12-25 17:51:55 +0800

    update

--- code0.cpp
+++ code0.cpp
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(){
-    printf("hello");
+int main()
+{
+    int a, b;
+    printf("请输入两个数：");
+    scanf("%d%d", &a, &b);
+    printf("%d", a + b);
+
     return 0;
 }
